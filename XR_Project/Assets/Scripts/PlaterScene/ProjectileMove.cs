1ff6a8a603d8eb5f747472b6b6104e820e1097bd
Sidan00 <sidan05084@gmail.com>
2023-05-03 20:52:42 +0900

    ui,sfx

--- XR_Project/Assets/Scripts/PlaterScene/ProjectileMove.cs
+++ XR_Project/Assets/Scripts/PlaterScene/ProjectileMove.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class ProjectileMove : MonoBehaviour
 {
-    public enum PROJECTILETYPE                                      //enum으로 타입 선언
-    {
-        PLAYER,
-        MONSTER
-    }
-
     public Vector3 launchDirection;                                 //발사 방향
-
-    public PROJECTILETYPE projectileType;
 
     private void FixedUpdate()
     {
@@ -20,35 +12,28 @@ public class ProjectileMove : MonoBehaviour
         transform.Translate(launchDirection * moveAmount);         //Translate 로 이동
     }
 
-    //private void OnCollisionEnter(Collision collision)             //충돌이 일어났을 경우
-    //{
-    //    Debug.Log(collision.gameObject.name);
-    //    if(collision.gameObject.tag == "Object")                   //Tag 값이 오브젝트인 경우
-    //    {
-    //        Destroy(this.gameObject);
-    //    }
+    private void OnCollisionEnter(Collision collision)             //충돌이 일어났을 경우
+    {
+        Debug.Log(collision.gameObject.name);
+        if(collision.gameObject.tag == "Object")                   //Tag 값이 오브젝트인 경우
+        {
+            Destroy(this.gameObject);
+        }
 
-    //    if (collision.gameObject.tag == "Monster")                   //Tag 값이
-    //    {
-    //        Destroy(this.gameObject);
-    //        collision.gameObject.GetComponent<Monster>().Damaged(1);
-    //    }
+        if (collision.gameObject.tag == "Monster")                   //Tag 값이
+        {
+            Destroy(this.gameObject);
+            collision.gameObject.GetComponent<Monster>().Damaged(1);
+        }
 
-    //}
+    }
 
     private void OnTriggerEnter(Collider other)                             //트리거 예약 함수
     {
-        if(other.CompareTag("Monster") && projectileType == PROJECTILETYPE.PLAYER)                                     //Tag를 검사한다.
+        if(other.CompareTag("Monster"))                                     //Tag를 검사한다.
         {
             Destroy(this.gameObject);
             other.gameObject.GetComponent<Monster>().Damaged(1);
         }
-
-        if (other.CompareTag("Player") && projectileType == PROJECTILETYPE.MONSTER)                                     //Tag를 검사한다.
-        {
-            Destroy(this.gameObject);
-            //other.gameObject.GetComponent<Monster>().Damaged(1);
-        }
-
     }
 }
